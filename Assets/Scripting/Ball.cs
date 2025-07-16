724f32318cdfdcf37b63736c06d8e2ada338e944
Elijah White <elijah.white1313@gmail.com>
2025-07-15 22:05:04 -0500

    Made a new Script for defining character, Fixed names in Unity. Finalized the ball code.

--- Assets/Scripting/Ball.cs
+++ Assets/Scripting/Ball.cs
@@ -5,13 +5,12 @@ public class Ball : MonoBehaviour
 {
     public enum CollisionTag
     {
-        ScoreWall,
         BounceWall,
-        Player
+        Player,
+        ScoreWall
     }
     [SerializeField] private float ballSpeed = 8f;
-    [SerializeField] private List<string> tags;
-    [SerializeField] private string otherTag;
+    [SerializeField] private List<string> collisionTags;
     private Vector2 ballDirection;
 
     [SerializeField] private AudioSource audioSource;
@@ -28,7 +27,7 @@ public class Ball : MonoBehaviour
     {
         transform.Translate(ballDirection * ballSpeed * Time.deltaTime);
     }
-
+    //Reset ball resets the ball to its base position at a random direction
     private void ResetBall()
     {
         transform.position = Vector2.zero;
@@ -36,17 +35,18 @@ public class Ball : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag(tags[(int)CollisionTag.ScoreWall]))
+        if (other.CompareTag(collisionTags[(int)CollisionTag.ScoreWall]))
         {
             audioSource.PlayOneShot(goalCollide);
             ResetBall();
+            GameManager.IncrementScore(other.GetComponent<ScoreWall>().scoringPlayer);
         }
-        else if (other.CompareTag(tags[(int)CollisionTag.BounceWall]))
+        else if (other.CompareTag(collisionTags[(int)CollisionTag.BounceWall]))
         {
             audioSource.PlayOneShot(wallCollide);
             ballDirection.y = -ballDirection.y;
         }
-        else if (other.CompareTag(tags[(int)CollisionTag.Player]))
+        else if (other.CompareTag(collisionTags[(int)CollisionTag.Player]))
         {
             audioSource.PlayOneShot(playerCollide);
             ballDirection.x = -ballDirection.x;
